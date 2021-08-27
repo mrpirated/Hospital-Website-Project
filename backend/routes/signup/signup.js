154fe700e0b0746e9919042593c0d4f5ef0b6580
Aditya Darji <darjiaditya2000@gmail.com>
2021-08-27 21:28:23 +0530

    login signup done with controller

--- backend/routes/signup/signup.js
+++ backend/routes/signup/signup.js
@@ -4,6 +4,7 @@ import { check, validationResult } from "express-validator";
 import connection from "../../dbconn/db";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
+import signup from "../../controllers/signup";
 dotenv.config();
 const bcrypt = require("bcrypt");
 router.post(
@@ -18,61 +19,7 @@ router.post(
 			min: 6,
 		}),
 	],
-	async (req, res) => {
-		const errors = validationResult(req);
-		if (!errors.isEmpty()) {
-			console.log(errors);
-			return res
-				.status(ErrorCode.HTTP_BAD_REQ)
-				.json({ errors: errors.array() });
-		}
-		try {
-			var value = {
-				first_name: req.body.first_name,
-				last_name: req.body.last_name,
-				dob: req.body.dob,
-				gender: req.body.gender,
-				address: req.body.address,
-				email: req.body.email,
-				phone: req.body.phone,
-				password: req.body.password,
-			};
-			value.password = await bcrypt.hash(req.body.password, 10);
-
-			console.log(value.password);
-			var patient_id;
-			var q = connection.query(
-				"INSERT INTO patient SET ?",
-				value,
-				(err, results, fields) => {
-					//console.log("jere");
-					if (err) console.log(err);
-					else {
-						patient_id = results.insertId;
-						//console.log(patient_id);
-						//console.log(t);
-					}
-				}
-			);
-			// console.log(q);
-			// console.log(value);
-			// jwt.sign(
-			// 	{
-			// 		data: value,
-			// 		patient_id,
-			// 	},
-			// 	process.env.SECRET_KEY,
-			// 	{ expiresIn: 60 * 60 * 24 * 30 },
-			// 	(err, token) => {
-			// 		//console.log(token);
-			// 		res.send(token);
-			// 	}
-			// );
-			//res.send(jwttoken);
-		} catch {
-			(err) => console.log(err);
-		}
-	}
+	signup
 );
 
 export default router;
