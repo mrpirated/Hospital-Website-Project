5767eb6922933e2d780481a225f7445366e759fa
Deepesh Rathi <deepeshrathi5050@gmail.com>
2021-08-27 18:54:00 +0530

    doing signup

--- backend/routes/signup/signup.js
+++ backend/routes/signup/signup.js
@@ -2,17 +2,22 @@ import { Router } from "express";
 const router = Router();
 import { check, validationResult } from "express-validator";
 import connection from "../../dbconn/db";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
 const bcrypt = require("bcrypt");
-router.post("/signup", [
-	check("first_name", "Name is required").not().isEmpty(),
-	//check("first_name", "Name is required").not().isEmpty(),
-	check("email", "Valid Email required").isEmail(),
-	check(
-		"password",
-		"Please enter a password with 6 or more characters"
-	).isLength({
-		min: 6,
-	}),
+router.post(
+	"/signup",
+	[
+		check("first_name", "Name is required").not().isEmpty(),
+		check("email", "Valid Email required").isEmail(),
+		check(
+			"password",
+			"Please enter a password with 6 or more characters"
+		).isLength({
+			min: 6,
+		}),
+	],
 	async (req, res) => {
 		const errors = validationResult(req);
 		if (!errors.isEmpty()) {
@@ -21,15 +26,7 @@ router.post("/signup", [
 				.status(ErrorCode.HTTP_BAD_REQ)
 				.json({ errors: errors.array() });
 		}
-		//console.log(req);
-		//signup;
-		// const salt = await bcrypt.genSalt(10);
-		// const hashedPassword = await bcrypt.hash(req.password, salt);
 		try {
-			bcrypt.hash(req.password, 10, (err, hash) => {
-				req.body.password = hash;
-			});
-			//req.password = hashedPassword;
 			var value = {
 				first_name: req.body.first_name,
 				last_name: req.body.last_name,
@@ -40,23 +37,42 @@ router.post("/signup", [
 				phone: req.body.phone,
 				password: req.body.password,
 			};
-			console.log(value);
-			var q = await connection.query(
+			value.password = await bcrypt.hash(req.body.password, 10);
+
+			console.log(value.password);
+			var patient_id;
+			var q = connection.query(
 				"INSERT INTO patient SET ?",
 				value,
 				(err, results, fields) => {
 					//console.log("jere");
 					if (err) console.log(err);
 					else {
-						console.log(results);
+						patient_id = results.insertId;
+						//console.log(patient_id);
+						//console.log(t);
 					}
 				}
 			);
-			//console.log(q.sql);
+			// console.log(q);
+			// console.log(value);
+			// jwt.sign(
+			// 	{
+			// 		data: value,
+			// 		patient_id,
+			// 	},
+			// 	process.env.SECRET_KEY,
+			// 	{ expiresIn: 60 * 60 * 24 * 30 },
+			// 	(err, token) => {
+			// 		//console.log(token);
+			// 		res.send(token);
+			// 	}
+			// );
+			//res.send(jwttoken);
 		} catch {
 			(err) => console.log(err);
 		}
-	},
-]);
+	}
+);
 
 export default router;
