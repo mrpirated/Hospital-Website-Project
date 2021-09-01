7ccfeb2bb5a7b408b22216b1f66930af411dcae7
Aditya Darji <darjiaditya2000@gmail.com>
2021-09-01 23:24:35 +0530

    login_doctor done

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import login from "./routes/login/login";
+import login_doctor from "./routes/login/login_doctor";
 import signup from "./routes/signup/signup";
 import signup_doctor from "./routes/signup/signup_doctor";
 import connection from "./dbconn/db";
@@ -10,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(json({ extended: false }));
-app.use("/api", login, signup, signup_doctor);
+app.use("/api", login, signup, signup_doctor, login_doctor);
 app.listen(PORT, () => {
 	console.log("✨✨ Running on port " + PORT);
 });
---
+++ backend/routes/login/login_doctor.js
@@ -0,0 +1,22 @@
+import { Router } from "express";
+const router = Router();
+import connection from "../../dbconn/db";
+import { check, validationResult } from "express-validator";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+import login_doctor from '../../controllers/login_doctor';
+const bcrypt = require("bcrypt");
+
+router.post("/login_doctor", [
+    check("email", "Valid Email required").isEmail(),
+    check(
+        "password",
+        "Please enter a valid password"
+    ).isLength({
+        min: 6,
+    }),
+],
+    login_doctor);
+
+export default router;
