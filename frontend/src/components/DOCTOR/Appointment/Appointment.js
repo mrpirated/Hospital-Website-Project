f8a13cbbbeb846ba78e6c0b88ef4731721406407
Aditya Darji <darjiaditya2000@gmail.com>
2021-09-20 17:04:45 +0530

    Doctor appointment started

--- frontend/src/components/DOCTOR/Appointment/Appointment.js
+++ frontend/src/components/DOCTOR/Appointment/Appointment.js
@@ -1,7 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
+import { useHistory } from "react-router";
+import {Button} from "react-bootstrap";
+//import {DayPilotScheduler} from "daypilot-pro-react";
+import {Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, EventSettingsModel, ViewsDirective, ViewDirective, ResourcesDirective, ResourceDirective} from "@syncfusion/ej2-react-schedule";
 
 function Appointment() {
-	return <div>Doctor_Appointment</div>;
+	const history = useHistory();
+	
+	const onclickfunc = () => {
+		console.log("HELLO");
+	}
+	var localData = {
+		dataSource: [{
+			Subject: "Hello",
+			EndTime: new Date(2021, 8, 20, 15, 0),
+			StartTime: new Date(2021,8, 20, 14, 0),
+		},
+		{
+			Subject: "Hello",
+			EndTime: new Date(2021, 8, 20, 16, 0),
+			StartTime: new Date(2021,8, 20, 15, 0)
+		}]
+	}
+
+	return (
+		<div>
+		{console.log(new Date(2021, 8, 20, 14, 0))}
+			<ScheduleComponent currentView='Day' eventSettings={localData} readonly={true}>
+				<ViewsDirective>
+					<ViewDirective option='Day'/>
+				</ViewsDirective>
+				<Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
+			</ScheduleComponent>
+		</div>
+	);
 }
 
 export default Appointment;
