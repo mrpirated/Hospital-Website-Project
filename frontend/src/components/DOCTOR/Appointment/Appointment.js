4a32e35f50fba8dbbcf907965f6f2e6155d7def1
Aditya Darji <darjiaditya2000@gmail.com>
2021-09-20 18:48:11 +0530

    Doctor Appointment done

--- frontend/src/components/DOCTOR/Appointment/Appointment.js
+++ frontend/src/components/DOCTOR/Appointment/Appointment.js
@@ -1,32 +1,38 @@
 import React, {useState} from "react";
 import { useHistory } from "react-router";
 import {Button} from "react-bootstrap";
-//import {DayPilotScheduler} from "daypilot-pro-react";
+import { Link } from 'react-router-dom';
 import {Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, EventSettingsModel, ViewsDirective, ViewDirective, ResourcesDirective, ResourceDirective} from "@syncfusion/ej2-react-schedule";
 
 function Appointment() {
 	const history = useHistory();
-	
 	const onclickfunc = () => {
-		console.log("HELLO");
+		console.log("Hello");
 	}
 	var localData = {
 		dataSource: [{
+			id: 1,
 			Subject: "Hello",
 			EndTime: new Date(2021, 8, 20, 15, 0),
 			StartTime: new Date(2021,8, 20, 14, 0),
 		},
 		{
+			id: 2,
 			Subject: "Hello",
 			EndTime: new Date(2021, 8, 20, 16, 0),
 			StartTime: new Date(2021,8, 20, 15, 0)
-		}]
+		}],
 	}
+
+	const onPopupOpen = (args) => {
+        history.push("/home", {data: args.data});	
+    }
 
 	return (
 		<div>
-		{console.log(new Date(2021, 8, 20, 14, 0))}
-			<ScheduleComponent currentView='Day' eventSettings={localData} readonly={true}>
+			{/* <ButtonComponent id='btn1' title='Click to open Editor' onClick={this.onClickButton1.bind(this)}>Click to open Editor</ButtonComponent> */}
+			<ScheduleComponent currentView='Day' eventSettings={localData} 
+			readonly={true} popupOpen={onPopupOpen}>
 				<ViewsDirective>
 					<ViewDirective option='Day'/>
 				</ViewsDirective>
