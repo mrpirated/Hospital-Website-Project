a8139cb432e3543e5d7927f058000bbfdef1da0f
Deepesh Rathi <deepeshrathi5050@gmail.com>
2021-09-22 02:09:45 +0530

    available schedule done

--- backend/controllers/admin/helpers.js
+++ backend/controllers/admin/helpers.js
@@ -1,23 +1,56 @@
 export const parseDateArray = (data) => {
 	data = data.map((dt) => {
 		return {
-			start_time: Date(dt.start_time),
-			end_time: Date(dt.end_time),
+			start_time: Date.parse(dt.start_time),
+			end_time: Date.parse(dt.end_time),
 		};
 	});
 	return data;
 };
 
 export const getAvailableTime = (sch, app) => {
+	if (sch.length == 0 || app.length == 0) return sch;
 	var schit = 0,
 		appit = 0;
 	var ans = [];
+	var st = sch[0].start_time;
 	var tp = sch[0];
+
+	console.log(sch);
+	console.log(app);
 	while (schit < sch.length && appit < app.length) {
-		if (sch[schit].start_time === app[appit].start_time) {
-			if (sch[schit].end_time > app[appit].end_time) {
+		if (
+			st <= app[appit].start_time &&
+			sch[schit].end_time >= app[appit].start_time
+		) {
+			//console.log(st);
+			if (st != app[appit].start_time) {
+				tp.start_time = st;
+				tp.end_time = app[appit].start_time;
+				ans.push(tp);
+				st = app[appit].end_time;
+				appit++;
+			} else {
+				st = app[appit].end_time;
+			}
+		} else {
+			if (st != sch[schit].end_time) {
+				tp.start_time = st;
+				tp.end_time = sch[schit].end_time;
+				ans.push(tp);
+				schit++;
+				if (schit < sch.length) st = sch[schit].start_time;
 			}
 		}
 	}
+	//console.log(typeof ans[0].start_time);
+	ans = ans.map((a) => {
+		var st = new Date(a.start_time);
+		var et = new Date(a.end_time);
+		return {
+			start_time: st.toString(),
+			end_time: et.toString(),
+		};
+	});
 	return ans;
 };
