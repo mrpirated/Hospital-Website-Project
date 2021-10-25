81638faa597eaff52d542e02d4f4970942195a82
Deepesh Rathi <deepeshrathi5050@gmail.com>
2021-10-25 19:48:39 +0530

    added hostname

--- frontend/index.js
+++ frontend/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT;
+const HOST_NAME = process.env.HOST_NAME;
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", (req, res) => {
@@ -10,5 +11,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(PORT, "0.0.0.0", (req, res) => {
-	console.log("Frontend is Running");
+	console.log(`Frontend is Running at http://${HOST_NAME}:${PORT}:`);
 });
