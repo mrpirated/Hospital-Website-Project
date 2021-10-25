6a4fabf6a4aa4932a1a5d59313e94e8b127df18a
Deepesh Rathi <deepeshrathi5050@gmail.com>
2021-10-25 19:43:22 +0530

    added dotenv

--- frontend/index.js
+++ frontend/index.js
@@ -1,13 +1,14 @@
+require("dotenv");
 const express = require("express");
 const path = require("path");
 const app = express();
-
+const PORT = process.env.PORT;
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(5905, "0.0.0.0", (req, res) => {
+app.listen(PORT, "0.0.0.0", (req, res) => {
 	console.log("Frontend is Running");
 });
