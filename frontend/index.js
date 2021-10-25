6a3b1f7a30e58b4cb9d86c2603024512ca60ae94
Deepesh Rathi <deepeshrathi5050@gmail.com>
2021-10-25 19:06:41 +0530

    frontend change

--- frontend/index.js
+++ frontend/index.js
@@ -8,4 +8,6 @@ app.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
-app.listen(5905);
+app.listen(5905, "0.0.0.0", (req, res) => {
+	console.log("Frontend is Running");
+});
