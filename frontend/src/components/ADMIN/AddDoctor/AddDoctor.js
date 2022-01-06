e2cec46f543ddf9550a60fe0c78c09fbffd5a8d4
Aditya Darji <darjiaditya2000@gmail.com>
2022-01-06 22:13:45 +0530

    add doctors component changes

--- frontend/src/components/ADMIN/AddDoctor/AddDoctor.js
+++ frontend/src/components/ADMIN/AddDoctor/AddDoctor.js
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
 import { Form, Row, Col, Button } from "react-bootstrap";
 import { useHistory } from "react-router";
-import "./Signup.css";
 import format from "date-fns/format";
 import DateFnsUtils from '@date-io/date-fns';
 import "react-datepicker/dist/react-datepicker.css";
 import {KeyboardDatePicker , MuiPickersUtilsProvider } from '@material-ui/pickers';
 import addDoctorAPI from "../../../api/addDoctorAPI";
 import { useSelector } from "react-redux";
+import PhoneInput from "react-phone-number-input";
 
 function AddDoctor() {
     const [first_name, setFirstName] = useState("");
@@ -62,106 +62,123 @@ function AddDoctor() {
 	};
 
     return (
-        <div>
-			<div className='Signup'>
-                <h3 className='FormHeading'>Enter Details of Doctor</h3>
+			<div className='addDoctorForm' style={{ backgroundColor: "#ffffe6", paddingBottom: "20px" }}>
 				<Form onSubmit={handleSubmit}>
-					<Row>
-						<Form.Group as={Col}>
-							<Form.Label>First Name</Form.Label>
+					<div>
+						<h2 id='headerTitle'>Enter Details of Doctor</h2>
+						<div style={{ width: "100%", overflow: "hidden" }}>
+							<div className='row' style={{ width: "50%", float: "left" }}>
+								<label>First Name</label>
+								<input
+									type='text'
+									value={first_name}
+									// placeholder="Enter your First Name"
+									onChange={(e) => setFirstName(e.target.value)}
+								/>
+							</div>
+							<div className='row' style={{ float: "right", width: "50%" }}>
+								<label>Last Name</label>
+								<input
+									// placeholder="Enter your Last Name"
+									type='text'
+									value={last_name}
+									onChange={(e) => setLastName(e.target.value)}
+								/>
+							</div>
+						</div>
+						<div className='row'>
+								<label>Phone Number</label>
+								<PhoneInput
+									// placeholder="Enter phone number"
+									defaultCountry='IN'
+									value={phone}
+									style={{ width: "85%" }}
+									onChange={setPhone}
+								/>
+							</div>
+						<div className='row'>
+							<label>Email</label>
+							<input
+								// placeholder="Enter your Last Name"
+								type='email'
+								value={email}
+								onChange={(e) => setEmail(e.target.value)}
+							/>
+						</div>
+						<div className='row'>
+							<label>Password</label>
+							<input
+								// placeholder="Enter your Last Name"
+								type='password'
+								value={password}
+								onChange={(e) => setPassword(e.target.value)}
+							/>
+						</div>
+						<div className='row'>
+							<label>Confirm Password</label>
+							<input
+								// placeholder="Enter your Last Name"
+								type='password'
+								value={confirmPassword}
+								onChange={(e) => setConfirmPassword(e.target.value)}
+							/>
+						</div>
+						{/* <Form.Group size='lg'>
+							<Form.Label>Date Of Birth</Form.Label>
+							<MuiPickersUtilsProvider utils={DateFnsUtils}>
+								<KeyboardDatePicker
+									autoOk
+									variant="inline"
+									inputVariant="outlined"
+									format="dd/MM/yyyy"
+									value={dob}
+									onChange={(date) => setDob(date)}
+									InputAdornmentProps={{ position: "start" }}
+								/>
+							</MuiPickersUtilsProvider>
+							
+							{/* <DatePicker selected={dob} onChange={(date) => setDob(date)} />
+						</Form.Group>
+						<Form.Group size='lg'>
+							<Form.Label>Gender</Form.Label>
+							<Form.Control
+								as='select'
+								custom
+								onChange={(e) => setGender(e.target.value)}
+							>
+								<option value='PreferNotToSay'>Prefer Not To Say</option>
+								<option value='Male'>Male</option>
+								<option value='Female'>Female</option>
+							</Form.Control>
+						</Form.Group>
+						<Form.Group size='lg'>
+							<Form.Label>Address</Form.Label>
 							<Form.Control
 								type='text'
-								value={first_name}
-								onChange={(e) => setFirstName(e.target.value)}
+								value={address}
+								onChange={(e) => setAddress(e.target.value)}
 							/>
 						</Form.Group>
-						<Form.Group as={Col}>
-							<Form.Label>Last Name</Form.Label>
+						<Form.Group size='lg'>
+							<Form.Label>Phone</Form.Label>
 							<Form.Control
 								type='text'
-								value={last_name}
-								onChange={(e) => setLastName(e.target.value)}
+								value={phone}
+								onChange={(e) => setPhone(e.target.value)}
 							/>
-						</Form.Group>
-					</Row>
-					<Form.Group size='lg' controlId='email'>
-						<Form.Label>Email</Form.Label>
-						<Form.Control
-							autoFocus
-							type='email'
-							value={email}
-							onChange={(e) => setEmail(e.target.value)}
-						/>
-					</Form.Group>
-					<Form.Group size='lg' controlId='password'>
-						<Form.Label>Password</Form.Label>
-						<Form.Control
-							type='password'
-							value={password}
-							onChange={(e) => setPassword(e.target.value)}
-						/>
-					</Form.Group>
-					<Form.Group size='lg' controlId='password'>
-						<Form.Label>
-							Confirm Password
-							{flag ? "*Password and Confirm Password should match" : ""}
-						</Form.Label>
-						<Form.Control
-							type='password'
-							value={confirmPassword}
-							onChange={(e) => setConfirmPassword(e.target.value)}
-						/>
-					</Form.Group>
-					<Form.Group size='lg'>
-						<Form.Label>Date Of Birth</Form.Label>
-						<MuiPickersUtilsProvider utils={DateFnsUtils}>
-							<KeyboardDatePicker
-								autoOk
-								variant="inline"
-								inputVariant="outlined"
-								format="dd/MM/yyyy"
-								value={dob}
-								onChange={(date) => setDob(date)}
-								InputAdornmentProps={{ position: "start" }}
-							/>
-						</MuiPickersUtilsProvider>
-						
-						{/* <DatePicker selected={dob} onChange={(date) => setDob(date)} /> */}
-					</Form.Group>
-					<Form.Group size='lg'>
-						<Form.Label>Gender</Form.Label>
-						<Form.Control
-							as='select'
-							custom
-							onChange={(e) => setGender(e.target.value)}
-						>
-							<option value='PreferNotToSay'>Prefer Not To Say</option>
-							<option value='Male'>Male</option>
-							<option value='Female'>Female</option>
-						</Form.Control>
-					</Form.Group>
-					<Form.Group size='lg'>
-						<Form.Label>Address</Form.Label>
-						<Form.Control
-							type='text'
-							value={address}
-							onChange={(e) => setAddress(e.target.value)}
-						/>
-					</Form.Group>
-					<Form.Group size='lg'>
-						<Form.Label>Phone</Form.Label>
-						<Form.Control
-							type='text'
-							value={phone}
-							onChange={(e) => setPhone(e.target.value)}
-						/>
-					</Form.Group>
-					<Button block size='lg' type='submit' disabled={!validateForm()}>
-						Signup
-					</Button>
+						</Form.Group> */}
+						<div id='button' class='row'>
+							<button
+								style={{ width: "45%", fontSize: "15px" }}
+								type='submit'
+								disabled={!validateForm()}
+							>
+								Submit
+							</button>
+						</div>
+					</div>
 				</Form>
 			</div>
-		</div>
     )
 }
 
