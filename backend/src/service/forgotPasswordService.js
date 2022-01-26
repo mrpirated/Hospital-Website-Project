13f6938d7eb3aa661caab10c25c504d8020324c6
Deepesh Rathi <deepeshrathi5050@gmail.com>
2022-01-27 00:36:15 +0530

    forgot password implemented

--- backend/src/service/forgotPasswordService.js
+++ backend/src/service/forgotPasswordService.js
@@ -3,10 +3,19 @@ const debug = dbg("service:forgotPasswordService");
 import twilioOTP from "../controllers/twilioOTP";
 import updatePassword from "../data/updatePassword";
 import hashPassword from "../controllers/hashPassword";
+import checkIfUserExistsPhone from "../data/checkIfUserExistsPhone";
 const forgotPasswordService = async ({ user }) => {
-	return await twilioOTP(user)
+	var user_id;
+	return await checkIfUserExistsPhone(user)
 		.then((response) => {
-			if (user.code) {
+			if (user.type === "admin") user_id = response.data.user.admin_id;
+			else if (user.type === "doctor") user_id = response.data.user.doctor_id;
+			else if (user.type === "patient") user_id = response.data.user.patient_id;
+
+			return twilioOTP(user);
+		})
+		.then((response) => {
+			if (user.otp) {
 				if (response === "approved") {
 					return hashPassword(user.password);
 				} else {
@@ -17,7 +26,11 @@ const forgotPasswordService = async ({ user }) => {
 			}
 		})
 		.then((response) => {
-			return;
+			if (user.otp === undefined) {
+				return response;
+			}
+
+			return updatePassword(user.type, response, user_id);
 		})
 		.catch((error) => {
 			debug(error);
