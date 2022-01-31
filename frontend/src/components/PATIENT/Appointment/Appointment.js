0f31a8e2be60abaee1cede561d82b296a1a53ae1
Aditya Darji <darjiaditya2000@gmail.com>
2022-01-31 11:58:14 +0530

    Display Appointments and cases in cards view. Fixes #23

--- frontend/src/components/PATIENT/Appointment/Appointment.js
+++ frontend/src/components/PATIENT/Appointment/Appointment.js
@@ -4,15 +4,24 @@ import { Switch, useHistory } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import { setLoading } from "../../../store/auth";
 import patientCaseAPI from "../../../api/patientCaseAPI";
-import { Card, Table, Button } from "react-bootstrap";
+import { Card, Table, Button,  ListGroup, ListGroupItem, Image,} from "react-bootstrap";
 import moment from "moment";
 //import "./Appointment.css";
 
 export default function Appointment(props) {
 	const auth = useSelector((state) => state.auth);
+	const dispatch = useDispatch();
 	const history = useHistory();
 	const [cases, setCases] = useState([]);
-	const dispatch = useDispatch();
+	const [displayCases, setDisplayCases] = useState([]);
+	const [pages, setPages] = useState(1);
+	const [currentPage, setCurrentPage] = useState(1);
+	const dataLimit = 10, pageLimit = 5;
+
+	function sleep(ms) {
+		return new Promise(resolve => setTimeout(resolve, ms));
+	}
+
 	useEffect(() => {
 		sessionStorage.setItem("lastPage", "/patient/appointment");
 		// if (!(auth.isauth && auth.type === 0)) {
@@ -28,15 +37,43 @@ export default function Appointment(props) {
 			if (res.success) {
 				console.log(res.data.cases);
 				setCases(res.data.cases);
+				setDisplayCases(res.data.cases);
+				setPages(Math.ceil(res.data.cases.length / dataLimit));
 			} else {
 				// alert(res.data.msg + "\nYou will be redirected to Home.");
 				setTimeout(history.push("/patient/appointment"), 0);
 			}
-			dispatch(setLoading({ loading: false }));
+			sleep(1000).then(() => {
+				dispatch(setLoading({ loading: false }));
+			})
 		});
 
 		//setTimeout(patientFunc(), 100);
 	}, []);
+
+	function goToNextPage() {
+		setCurrentPage((page) => page + 1);
+	}
+
+	function goToPreviousPage() {
+		setCurrentPage((page) => page - 1);
+	}
+
+	function changePage(event) {
+		const pageNumber = Number(event.target.textContent);
+		setCurrentPage(pageNumber);
+	}
+
+	const getPaginatedData = () => {
+		const startIndex = currentPage * dataLimit - dataLimit;
+		const endIndex = startIndex + dataLimit;
+		return displayCases.slice(startIndex, endIndex);
+	};
+
+	const getPaginationGroup = () => {
+		let start = Math.floor((currentPage - 1) / pageLimit) * pageLimit;
+		return new Array(pageLimit).fill().map((_, idx) => start + idx + 1);
+	};
 
 	return (
 		<div>
@@ -50,7 +87,7 @@ export default function Appointment(props) {
 					New Case
 				</Button>
 			</div>
-			<Table striped bordered hover responsive='lg'>
+			{/* <Table striped bordered hover responsive='lg'>
 				<thead style={{ textAlign: "center" }}>
 					<th>Case Description</th>
 					<th>Latest Appointment</th>
@@ -70,7 +107,7 @@ export default function Appointment(props) {
 						</tr>
 					))}
 				</tbody>
-			</Table>
+			</Table> */}
 			{/* <div id='card'>
 				<Card
 					className='appointment-addcard'
@@ -85,8 +122,8 @@ export default function Appointment(props) {
 						<Card.Text></Card.Text>
 					</Card.Body>
 				</Card>
-			</div>
-			{cases.map((c) => (
+			</div> */}
+			{/* {cases.map((c) => (
 				<div id='card' key={c.case_id}>
 					<Card
 						className='appointment'
@@ -101,6 +138,81 @@ export default function Appointment(props) {
 					</Card>
 				</div>
 			))} */}
+			<div>
+				<div>
+					{getPaginatedData().map((c) => (
+						<div id='cardDiv' key={c.case_id}>
+							<Card 
+								className='appointmentCard' 
+								onClick={() => {
+									history.push("/patient/myappointment", { case_details: c });
+								}}
+							>
+								<Card.Body>
+									<Card.Title>
+										<span>Case Id: {c.case_id}</span>
+									</Card.Title>
+
+									<ListGroup
+										style={{
+											fontSize: "0.8rem",
+										}}
+									>
+										<ListGroupItem>
+											<b>Description:</b> {c.case_description}
+										</ListGroupItem>
+										<ListGroupItem>
+											<b>Doctor:</b> {"Temp Data"}
+										</ListGroupItem>
+										<ListGroupItem>
+											<b>Speciality:</b> {"Temp Data"}
+										</ListGroupItem>
+										<ListGroupItem>
+											<b>Creation Date:</b> {"Temp Data"}
+										</ListGroupItem>
+									</ListGroup>
+								</Card.Body>
+							</Card>
+						</div>
+					))}
+				</div>
+				<div
+					className='pagination'
+					style={{ paddingBottom: "20px", paddingTop: "20px" }}
+				>
+					{/* previous button */}
+					<button
+						onClick={goToPreviousPage}
+						className={`prev ${currentPage === 1 ? "disabled" : ""}`}
+						disabled={currentPage === 1}
+					>
+						prev
+					</button>
+
+					{/* show page numbers */}
+					{getPaginationGroup().map((item, index) => (
+						<button
+							key={index}
+							onClick={changePage}
+							className={`${index >= pages ? "disabled" : ""} paginationItem ${
+								currentPage === item ? "active" : null
+							}`}
+							disabled={index >= pages}
+						>
+							<span>{item}</span>
+						</button>
+					))}
+
+					{/* next button */}
+					<button
+						onClick={goToNextPage}
+						className={`next ${currentPage === pages ? "disabled" : ""}`}
+						disabled={currentPage === pages}
+					>
+						next
+					</button>
+				</div>
+			</div>
 		</div>
 	);
 }
