919bc23721476d6a10962acfd28bcbdafd6be647
Deepesh Rathi <deepeshrathi5050@gmail.com>
2022-02-01 02:51:13 +0530

    add component for updating patient details (gender, address, dob ) Fixes #8

--- frontend/src/components/PATIENT/Profile/Profile.js
+++ frontend/src/components/PATIENT/Profile/Profile.js
@@ -1,31 +1,63 @@
 import React, { useState } from "react";
-import { Nav } from "react-bootstrap";
+import { Nav, Tabs, Tab, Row, Col, NavItem } from "react-bootstrap";
+import ProfileInfo from "./ProfileInfo";
 //import { Link } from "react-router-dom";
 function Profile() {
 	const [key, setKey] = useState("profile");
 	return (
-		<div>
-			<div>
-				<Nav
-					className='col-md-12 d-none d-md-block sidebar'
-					onSelect={(selectedKey) => {
-						setKey(selectedKey);
-						console.log(selectedKey);
-					}}
+		<div className='profile'>
+			{/* <div>
+				<Tabs
+					id='tab'
+					activeKey={key}
+					onSelect={(k) => setKey(k)}
+					className='mb-3'
+					style={{ flexDirection: "column" }}
 				>
-					<Nav.Item>
-						<Nav.Link eventKey='profile'>Profile</Nav.Link>
-					</Nav.Item>
-					<Nav.Item>
-						<Nav.Link eventKey='questions'>Previous Diseases</Nav.Link>
-					</Nav.Item>
-					<Nav.Item>
-						<Nav.Link eventKey='update' disabled>
-							Update Personal Information
-						</Nav.Link>
-					</Nav.Item>
-				</Nav>
+					<Tab eventKey='profile' title='Profile'>
+						Profile
+					</Tab>
+					<Tab eventKey='profile2' title='profile2'>
+						New Tab
+					</Tab>
+				</Tabs>
+			</div> */}
+			<div style={{ textAlign: "center", padding: "20px" }}>
+				<h1>Profile</h1>
 			</div>
+			<Tab.Container
+				id='left-tabs-example'
+				activeKey={key}
+				onSelect={(k) => setKey(k)}
+			>
+				<Row style={{ flexDirection: "row" }}>
+					<Col sm={3}>
+						<div style={{ margin: "1%" }}>
+							<Nav variant='pills' className='flex-column'>
+								<Nav.Item>
+									<Nav.Link eventKey='profile'>Profile</Nav.Link>
+								</Nav.Item>
+								<Nav.Item>
+									<Nav.Link eventKey='login'>Login</Nav.Link>
+								</Nav.Item>
+								<Nav.Item>
+									<Nav.Link eventKey='phone'>Change Phone No.</Nav.Link>
+								</Nav.Item>
+							</Nav>
+						</div>
+					</Col>
+					<Col sm={9}>
+						<div style={{ margin: "1%" }}>
+							<Tab.Content>
+								<Tab.Pane eventKey='profile'>
+									<ProfileInfo />
+								</Tab.Pane>
+								<Tab.Pane eventKey='login'>Here</Tab.Pane>
+							</Tab.Content>
+						</div>
+					</Col>
+				</Row>
+			</Tab.Container>
 		</div>
 	);
 }
