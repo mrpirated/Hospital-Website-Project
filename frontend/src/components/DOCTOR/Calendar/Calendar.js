bd83517cdf37233a3987252a3a0992fb85db77e8
Aditya Darji <darjiaditya2000@gmail.com>
2022-02-01 22:21:11 +0530

    Calendar function created and appointment scheduler done

--- frontend/src/components/DOCTOR/Calendar/Calendar.js
+++ frontend/src/components/DOCTOR/Calendar/Calendar.js
@@ -5,39 +5,43 @@ import {Button} from "react-bootstrap";
 import { Link } from 'react-router-dom';
 import {Inject, ScheduleComponent, Day, Week, WorkWeek, Month, Agenda, EventSettingsModel, ViewsDirective, ViewDirective, ResourcesDirective, ResourceDirective} from "@syncfusion/ej2-react-schedule";
 import doctorAppointmentsAPI from "../../../api/doctorAppointmentsAPI";
+import SchedulerComponent from "../../SchedulerComponent";
 
 function Calendar() {
 	const auth = useSelector((state) => state.auth);
 	const history = useHistory();
 	const [localData, setLocalData] = useState({});
 	useEffect(() => {
-		if (!(auth.isauth && auth.type === 1)) {
+		if (!(auth.isauth && auth.type === "doctor")) {
 			history.push("/home");
 		}
-		else{
+		else
+		{
 			doctorAppointmentsAPI({
 				token: auth.token
-			}).then((res) => {
-				if (res.reply) {
-					var tmp1 = {dataSource: res.appointments};
-					// var tmp = [];
-					// console.log(res.appointments);
-					// for(let i = 0; i < res.appointments.length; i++){
-					// 	console.log(res.appointments[i].id, res.appointments[i].Subject);
-					// 	tmp.push({
-					// 		Id: res.appointments[i].id,
-					// 		Subject: res.appointments[i].Subject,
-					// 		StartTime: res.appointments[i].StartTime,
-					// 		EndTime: res.appointments[i].EndTime
-					// 		// StartTime: new Date(2021, 8, 21, 0,0),
-					// 		// EndTime: new Date(2021, 8, 21, 1,0),
-					// 	})
-					// }
-					// var tmp1 = {dataSource: tmp};
+			}).then((response) => {
+				if (response.success) {
+					var tmp = [];
+					console.log(response.data.appointments);
+					for(let i = 0; i < response.data.appointments.length; i++){
+						// console.log(response.data.appointments[i].id, response.data.appointments[i].Subject);
+						tmp.push({
+							Id: response.data.appointments[i].appointment_id,
+							Subject: response.data.appointments[i].patient_name,
+							StartTime: response.data.appointments[i].start_time,
+							EndTime: response.data.appointments[i].end_time,
+							Description:"DESCRIPTION: " + response.data.appointments[i].case_description + ", " +
+										"CASE ID: " + response.data.appointments[i].case_id + ", " +
+										"STATE: " + response.data.appointments[i].state,
+							// StartTime: new Date(2021, 8, 21, 0,0),
+							// EndTime: new Date(2021, 8, 21, 1,0),
+						})
+					}
+					var tmp1 = {dataSource: tmp};
 					setLocalData(tmp1);
 				} else {
-					alert(res.data.msg + "\nYou will be redirected to Home.");
-					setTimeout(history.push("/home"), 4000);
+					// alert(res.data.msg + "\nYou will be redirected to Home.");
+					// setTimeout(history.push("/home"), 4000);
 				}
 			})
 		}
@@ -51,20 +55,7 @@ function Calendar() {
 
 	return (
 		<div>
-			{/* <ButtonComponent id='btn1' title='Click to open Editor' onClick={this.onClickButton1.bind(this)}>Click to open Editor</ButtonComponent> */}
-			<ScheduleComponent currentView='Day' eventSettings={localData} 
-			readonly={true} popupOpen={onPopupOpen}>
-				<ViewsDirective>
-					<ViewDirective option='Day'/>
-					<ViewDirective option='Week'/>
-					<ViewDirective option='Month'/>
-				</ViewsDirective>
-				{/* <ViewsDirective>
-				</ViewsDirective>
-				<ViewsDirective>
-				</ViewsDirective> */}
-				<Inject services={[Day, Week, WorkWeek, Month, Agenda]} />
-			</ScheduleComponent>
+		<SchedulerComponent localData={localData} onPopupOpen={onPopupOpen}/>
 		</div>
 	);
 }
