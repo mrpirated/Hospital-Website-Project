c7e732a5d1902bad08c85144664297ec82e747bd
Deepesh Rathi <deepeshrathi5050@gmail.com>
2022-03-12 17:16:00 +0530

    checked for admin in get profile pic

--- backend/src/service/getProfilePicAdminService.js
+++ backend/src/service/getProfilePicAdminService.js
@@ -6,6 +6,9 @@ import getProfilePic from "../controllers/getProfilePic";
 const getProfilePicAdminSevice = async (token, { doctor_id }) => {
 	return await checkToken(token)
 		.then((response) => {
+			if (response.data.decoded.type !== "admin") {
+				return Promise.reject({ success: false, message: "Not Authorized" });
+			}
 			return getProfilePicFileName("doctor", doctor_id);
 		})
 		.then((response) => {
