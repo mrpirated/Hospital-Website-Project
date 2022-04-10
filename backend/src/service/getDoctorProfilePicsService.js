47aca41e26589e60366f6766ad5c827ba0313972
Deepesh Rathi <deepeshrathi5050@gmail.com>
2022-04-10 15:11:38 +0530

    profile pic tokencheck removed

--- backend/src/service/getDoctorProfilePicsService.js
+++ backend/src/service/getDoctorProfilePicsService.js
@@ -4,10 +4,11 @@ import checkToken from "../controllers/checkToken";
 import getDoctors from "../data/getDoctors";
 import getDoctorProfilePic from "../controllers/getDoctorProfilePic";
 const getDoctorProfilePicsService = async (token) => {
-	return await checkToken(token)
-		.then((response) => {
-			return getDoctors();
-		})
+	// return await checkToken(token)
+	// 	.then((response) => {
+	// 		return getDoctors();
+	// 	})
+	return await getDoctors()
 		.then((response) => {
 			return getDoctorProfilePic(response.data.doctor);
 		})
